a6394dbf9d1e117b3a94421d5bc64c0f60a4c78a
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-09 20:43:36 +0100

    basic location detail widget build out

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:locations/location_detail.dart';
 
 void main() {
   return runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Sup World'),
-      ),
-    ),
+    home: LocationDetail(),
   ));
 }
-
