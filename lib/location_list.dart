b8adad8cc2796602a9078857d304ae4807c279d5
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-10 19:07:04 +0100

    location_list.dart init

---
+++ lib/location_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:locations/models/location.dart';
+
+class LocationList extends StatelessWidget {
+  final List<Location> locations;
+
+  LocationList(this.locations, {Key key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
